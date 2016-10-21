270e99bf1477b840bdae1211ce40b26a50912096
widonwaker <hey.lol@live.it>
2016-10-21 13:12:08 +0200

    Update appodeal.js

--- js/appodeal.js
+++ js/appodeal.js
@@ -1,4 +1,7 @@
-          var appKey = "80729c3857e25a006c7c1006fd65af2a2e03d540f22a6dce";
+document.addEventListener('deviceready', onDeviceReady, false);
+function onDeviceReady() {  
+var appKey = "80729c3857e25a006c7c1006fd65af2a2e03d540f22a6dce";
             Appodeal.initialize(appKey, Appodeal.INTERSTITIAL | Appodeal.SKIPPABLE_VIDEO | Appodeal.BANNER);
             Appodeal.show(Appodeal.BANNER_BOTTOM);
             Appodeal.isLoaded(Appodeal.BANNER, function(result){});
+}
