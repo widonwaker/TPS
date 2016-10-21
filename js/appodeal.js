fe8f89236745637d1a7d9af8f390adaef0808aae
widonwaker <hey.lol@live.it>
2016-10-21 14:42:40 +0200

    Update appodeal.js

--- js/appodeal.js
+++ js/appodeal.js
@@ -5,11 +5,5 @@ var appKey = "80729c3857e25a006c7c1006fd65af2a2e03d540f22a6dce";
             Appodeal.initialize(appKey, Appodeal.INTERSTITIAL | Appodeal.SKIPPABLE_VIDEO | Appodeal.BANNER);
             Appodeal.isLoaded(Appodeal.BANNER, function(result){});
             Appodeal.show(Appodeal.BANNER_BOTTOM);
-            Appodeal.enableInterstitialCallbacks(true);
 
-document.addEventListener('onInterstitialLoaded', function(){});
-document.addEventListener('onInterstitialFailedToLoad', function(){});
-document.addEventListener('onInterstitialShown', function(){});
-document.addEventListener('onInterstitialClicked', function(){});
-document.addEventListener('onInterstitialClosed', function(){});
 }
