3151878ca6e64e48412ebf59caecec173e42d765
widonwaker <hey.lol@live.it>
2016-10-22 11:51:27 +0200

    Update appodeal.js

--- js/appodeal.js
+++ js/appodeal.js
@@ -1,9 +1,4 @@
 document.addEventListener('deviceready', onDeviceReady, false);
-document.addEventListener('onInterstitialLoaded', function(){});
-document.addEventListener('onInterstitialFailedToLoad', function(){});
-document.addEventListener('onInterstitialShown', function(){});
-document.addEventListener('onInterstitialClicked', function(){});
-document.addEventListener('onInterstitialClosed', function(){});
 function onDeviceReady() {  
 var appKey = "80729c3857e25a006c7c1006fd65af2a2e03d540f22a6dce";
             Appodeal.disableLocationPermissionCheck();
